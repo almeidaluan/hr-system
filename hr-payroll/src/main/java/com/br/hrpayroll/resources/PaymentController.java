c1a6c928c3a2135904a09cda97ecb9ae23f95b38
luan nascimento <almeida.luan099@gmail.com>
2020-11-28 22:20:19 -0300

    implementacao de um payment mockado

---
+++ hr-payroll/src/main/java/com/br/hrpayroll/resources/PaymentController.java
@@ -0,0 +1,25 @@
+package com.br.hrpayroll.resources;
+
+import com.br.hrpayroll.entities.Payment;
+import com.br.hrpayroll.services.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/payment")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+
+    @GetMapping("/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable long workerId, @PathVariable Integer days){
+        Payment payment = paymentService.getPayment(workerId,days);
+        return ResponseEntity.ok(payment);
+    }
+}
