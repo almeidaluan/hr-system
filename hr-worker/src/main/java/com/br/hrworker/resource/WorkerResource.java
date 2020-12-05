5732b7a82ff09c578c3a599586725da790ff7eaa
luan nascimento <almeida.luan099@gmail.com>
2020-12-05 13:58:57 -0300

    Adicionando Endpoint por query parameter para retornar worker

--- hr-worker/src/main/java/com/br/hrworker/resource/WorkerResource.java
+++ hr-worker/src/main/java/com/br/hrworker/resource/WorkerResource.java
@@ -4,10 +4,7 @@ import com.br.hrworker.entities.Worker;
 import com.br.hrworker.repository.WorkerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,6 +26,11 @@ public class WorkerResource {
 
     @GetMapping(value = "{id}")
     public ResponseEntity<Optional<Worker>> FindWorker(@PathVariable long id){
+        return ResponseEntity.ok(repository.findById(id));
+    }
+
+    @GetMapping(value = "/Param")
+    public ResponseEntity<Optional<Worker>> FindWorkerQueryParam(@RequestParam Long id){
         return ResponseEntity.ok(repository.findById(id));
     }
 }
